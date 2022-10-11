caf08417264b12db2c5b97529ca7908cdcbdfe58
Roy Noviantho <roy.noviantho@global.ntt>
2022-10-11 23:05:37 +0700

    20221011: update swagger, add func cleansing

--- swagger.py
+++ swagger.py
@@ -4,6 +4,13 @@ from flasgger import Swagger, LazyString, LazyJSONEncoder, swag_from
 import re
 import pandas as pd
 
+
+#import function cleansing
+from cleansing import *
+
+#import db
+import sqlite3 as sq
+
 app = Flask(__name__) #deklarasi Flask
 app.json_encoder = LazyJSONEncoder
 
@@ -59,15 +66,58 @@ def return_text():
 @swag_from("swagger_config_form.yml", methods = ['POST'])
 @app.route('/post_form/v1',methods = ['POST'])
 def post_form():
-    text = request.form["text"]
-    return jsonify({"hasil input: ":text})
+    result = request.form["text"]
+    print(type(result))
+    result = replace_ascii(result)
+    result = remove_special_char(result)
+    result = remove_punctuation(result)
+    result = remove_whitespace_LT(result)
+    result = remove_whitespace_multiple(result) 
+    """
+    result = replace_ascii(result["text"])
+    result = remove_special_char(result["text"])
+    result = remove_punctuation(result["text"])
+    result = remove_whitespace_LT(result["text"])
+    result = remove_whitespace_multiple(result["text"]) 
+    """
+    return jsonify({"hasil input: ":result})
 
 @swag_from("swagger_config_file.yml",methods =['POST'])
 @app.route("/post_file/v1",methods =['POST'])
 def post_file():
     file = request.files["file"]
     df = pd.read_csv(file, encoding='latin-1')
-    print(df.head())
+    df = pd.DataFrame(df['Tweet'])
+
+    #cleansing
+    #case fold 
+    df['Tweet_Clean'] = df['Tweet'].str.lower()
+    #replace ascii
+    df['Tweet_Clean'] = df['Tweet_Clean'].apply(replace_ascii)
+    #remove special
+    df['Tweet_Clean'] = df['Tweet_Clean'].apply(remove_special_char)
+    #remove_punctuation
+    df['Tweet_Clean']= df['Tweet_Clean'].apply(remove_punctuation)
+    #remove whitespace leading & trailing
+    df['Tweet_Clean']= df['Tweet_Clean'].apply(remove_whitespace_LT)
+    #remove multiple whitespace into single whitespace
+    df['Tweet_Clean'] = df['Tweet_Clean'].apply(remove_whitespace_multiple)
+    # remove single char
+    df['Tweet_Clean']= df['Tweet_Clean'].apply(remove_single_char)
+    #hapus url
+    df['Tweet_Clean'] = df['Tweet_Clean'].apply(remove_urls)
+    df_tweet = pd.DataFrame().assign(Before=df['Tweet'], After=df['Tweet_Clean'])
+    
+    #import ke db
+    data = df_tweet
+    sql_data = 'TRAINING.db' #- Creates DB names SQLite
+    conn = sq.connect(sql_data)
+    cur = conn.cursor()
+    cur.execute('''DROP TABLE IF EXISTS TRAINING''')
+    data.to_sql('TRAINING', conn, if_exists='replace', index=False) # - writes the pd.df to SQLIte DB
+    conn.commit()
+    conn.close()
+
     return jsonify({"halo": str(df['Tweet'][2])})
 
 
